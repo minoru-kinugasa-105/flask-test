962e16b9b60b93a934877aaebb497bb4ea50d834
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2024-09-24 15:37:57 +0900

    コードが全部消えていたので再度構築；；

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # SCSSの設定
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=12341, debug=True)
